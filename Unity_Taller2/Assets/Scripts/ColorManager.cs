8ce42c333102697a2e0103f6b6841808761e09e1
JuanSMarin2 <juansmarin269mail.com>
2025-03-27 12:55:04 -0500

    Implementacion ColorManager

---
+++ Unity_Taller2/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public static ColorManager Instance { get; private set; }
+
+    public event Action<Color> OnColorChanged;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetColorRed()
+    {
+        ChangeColor(Color.red);
+    }
+
+    public void SetColorBlue()
+    {
+        ChangeColor(Color.blue);
+    }
+
+    public void SetColorGreen()
+    {
+        ChangeColor(Color.green);
+    }
+
+    private void ChangeColor(Color newColor)
+    {
+        OnColorChanged?.Invoke(newColor);
+    }
+}
+
+public interface IColorObserver
+{
+    void OnColorChanged(Color newColor);
+}
