30b3c64e65e095fd798b36515941f935763614d7
DaniBeta_33 <danipipe344@gmail.com>
2025-04-03 21:56:09 -0500

    Implementacion del singleton en el Inventario

--- Proyecto 1/ConsoleApp1/InventarioJuegos.cs
+++ Proyecto 1/ConsoleApp1/InventarioJuegos.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class InventarioJuegos
+    public sealed class InventarioJuegos
     {
+        private static readonly InventarioJuegos instancia = new InventarioJuegos();
+        private List<Juego> juegos = new List<Juego>();
+
+        private InventarioJuegos() { }
+
+        public static InventarioJuegos Instancia => instancia;
+
+        public void AgregarJuego(Juego juego)
+        {
+            juegos.Add(juego);
+        }
+
+        public IEnumerable<Juego> ObtenerJuegos()
+        {
+            return juegos;
+        }
     }
 }
