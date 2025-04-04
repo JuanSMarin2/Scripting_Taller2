88416eb5f62044eca6e37a1bb813587fd4c2b345
jDiego30 <juan.maldonadot@upb.edu.co>
2025-04-04 11:26:40 -0500

    ejercicio1

---
+++ pract1/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+public sealed class Logger
+{
+    private static readonly Lazy<Logger> instance = new Lazy<Logger>(() => new Logger());
+    private static readonly object lockObj = new object();
+    private string logFilePath;
+
+    private Logger()
+    {
+        logFilePath = "log.txt";
+    }
+
+    public static Logger Instance => instance.Value;
+
+    public void Log(string message)
+    {
+        lock (lockObj)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                Console.WriteLine(message);
+                writer.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}");
+            }
+        }
+    }
+}
---
+++ pract1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Iniciando aplicación...");
+
+        Logger.Instance.Log("La aplicación ha iniciado.");
+        Logger.Instance.Log("Ejecutando una operación importante...");
+        Logger.Instance.Log("Operación completada con éxito.");
+
+        Console.WriteLine("Mensajes registrados en el archivo de log.");
+    }
+}
